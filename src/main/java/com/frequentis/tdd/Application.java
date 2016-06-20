218784e6673da5033a4d4197d7a8395aa7efea46
PISLAR Serghei <serghei.pislar@frequentis.com>
2016-06-20 21:15:21 +0300

    application setup

---
+++ src/main/java/com/frequentis/tdd/Application.java
@@ -0,0 +1,18 @@
+/*
+ * COPYRIGHT: FREQUENTIS AG. All rights reserved.
+ *            Registered with Commercial Court Vienna,
+ *            reg.no. FN 72.115b.
+ */
+package com.frequentis.tdd;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
