a785dead8a99ceb0050899b0b1a5b7231d98ccc1
PISLAR Serghei <serghei.pislar@frequentis.com>
2016-06-22 20:08:28 +0300

    removed assertion on data from integration tests

--- src/test/java/com/frequentis/tdd/TddIntegrationTest.java
+++ src/test/java/com/frequentis/tdd/TddIntegrationTest.java
@@ -8,7 +8,6 @@ package com.frequentis.tdd;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.util.Arrays;
-import java.util.List;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -30,11 +29,6 @@ import org.springframework.web.context.WebApplicationContext;
 import com.frequentis.tdd.data.Randoms;
 import com.frequentis.tdd.data.Users;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.hasItem;
-import static org.hamcrest.Matchers.hasProperty;
-import static org.hamcrest.Matchers.not;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -78,25 +72,18 @@ public class TddIntegrationTest {
         User user = Users.random();
         String userJson = json(user);
 
-        // When
-        MvcResult mvcResult = mockMvc.perform(post("/user/").contentType(contentType).content(userJson)).andExpect(status().isOk()).andReturn();
-
-        // Then
-        User actualUser = fromJson(mvcResult.getResponse().getContentAsString());
-        assertThat("Expected id to be set", actualUser.getId(), not(equalTo(0L)));
+        // When/Then
+        mockMvc.perform(post("/user/").contentType(contentType).content(userJson)).andExpect(status().isOk());
     }
 
     @Test
     public void getAll_usersPresent_respondsWithAListOfUsers() throws Exception {
         // Given
-        User user1 = prepareUserInRepository();
-        User user2 = prepareUserInRepository();
+        prepareUserInRepository();
+        prepareUserInRepository();
 
-        // When
-        MvcResult mvcResult = mockMvc.perform(get("/user/all").contentType(contentType)).andExpect(status().isOk()).andReturn();
-
-        // Then
-        assertThatResponseContainsUsers(mvcResult, user1, user2);
+        // When/Then
+        mockMvc.perform(get("/user/all").contentType(contentType)).andExpect(status().isOk());
     }
 
     @Test
@@ -104,12 +91,8 @@ public class TddIntegrationTest {
         // Given
         User user = prepareUserInRepository();
 
-        // When
-        MvcResult mvcResult = mockMvc.perform(get("/user/"+user.getId()).contentType(contentType)).andExpect(status().isOk()).andReturn();
-
-        // Then
-        User actualUser = fromJson(mvcResult.getResponse().getContentAsString());
-        assertThat("Expected user to match", actualUser, equalTo(user));
+        // When/Then
+        mockMvc.perform(get("/user/" + user.getId()).contentType(contentType)).andExpect(status().isOk());
     }
 
     @Test
@@ -118,12 +101,8 @@ public class TddIntegrationTest {
         User user = prepareUserInRepository();
         user.setLastName(Randoms.randomAlphanumeric("lastName_"));
 
-        // When
-        MvcResult mvcResult = mockMvc.perform(put("/user/").contentType(contentType).content(json(user))).andExpect(status().isOk()).andReturn();
-
-        // Then
-        User actualUser = fromJson(mvcResult.getResponse().getContentAsString());
-        assertThat("Expected user to match", actualUser, equalTo(user));
+        // When/Then
+        mockMvc.perform(put("/user/").contentType(contentType).content(json(user))).andExpect(status().isOk());
     }
 
     @Test
@@ -132,14 +111,7 @@ public class TddIntegrationTest {
         User user = prepareUserInRepository();
 
         // When/Then
-        mockMvc.perform(delete("/user/"+user.getId()).contentType(contentType)).andExpect(status().isOk());
-    }
-
-    private void assertThatResponseContainsUsers(final MvcResult mvcResult, final User... users) throws IOException {
-        List<User> actualUsers = listFromJson(mvcResult.getResponse().getContentAsString());
-        for (User user : users) {
-            assertThat("Expected actual result contains the user", actualUsers, hasItem(hasProperty("id", equalTo(user.getId()))));
-        }
+        mockMvc.perform(delete("/user/" + user.getId()).contentType(contentType)).andExpect(status().isOk());
     }
 
     private User prepareUserInRepository() throws Exception {
@@ -157,10 +129,5 @@ public class TddIntegrationTest {
     protected User fromJson(String json) throws IOException {
         MockHttpInputMessage mockHttpInputMessage = new MockHttpInputMessage(json.getBytes());
         return (User) this.mappingJackson2HttpMessageConverter.read(User.class, mockHttpInputMessage);
-    }
-
-    protected List<User> listFromJson(String json) throws IOException {
-        MockHttpInputMessage mockHttpInputMessage = new MockHttpInputMessage(json.getBytes());
-        return Arrays.asList((User[]) this.mappingJackson2HttpMessageConverter.read(User[].class, mockHttpInputMessage));
     }
 }
